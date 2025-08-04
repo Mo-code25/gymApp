e09ab0bef80146e486bf018e1c388054b5cf742d
Mo-code25 <mohamedfekri073@gmail.com>
2025-08-04 17:07:24 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+	import "../app.css";
+	import Footer from '../components/Footer.svelte';
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
+
+<Footer/>
+
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Hero from "../components/Hero.svelte";
+</script>
+
+<h1 class="text-green-400">Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
+
+<h1 class="text-1xl font-bold underline">Hello world!</h1>
+
+<main class="flex flex-col">
+    <Hero />
+</main>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+    :global(html) {
+        background-color: theme(--color-gray-100);
+    }
+</style>
