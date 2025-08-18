ab38c7c7b57b3f9421d6f179fbd550ab81a384f1
Mo-code25 <mohamedfekri073@gmail.com>
2025-08-18 16:34:41 +0200

    description, get started button, rating

---
+++ src/components/CTAs.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<div class="flex items-center gap-4">
+    <button class="specialBtn">
+        <p class="textbase sm:text-lg md:text-xl">Contact Us</p>
+    </button>
+    <button class="specialBtnDark">
+        <p class="textbase sm:text-lg md:text-xl">Get Started &rarr;</p>
+    </button>
+</div>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,7 +3,7 @@
 
 <header class="flex flex-col relative z-20">
     <div
-        class="max-w-[1400px] mx-auto w-full flex items-center justify between p-4 py-6"
+        class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6"
     >
         <a href="/">
             <h1 class="font-semibold">
@@ -17,11 +17,11 @@
         </button>
 
         <nav class="hidden md:flex items-center gap-4 lg:gap-6 ml-auto">
-            <a>Product</a>
-            <a>Reviews</a>
-            <a>FAQs</a>
+            <a href="#product" class="duration-200 hover:text-indigo-400 cursor-pointer">Product</a>
+            <a href="#reviews" class="duration-200 hover:text-indigo-400 cursor-pointer">Reviews</a>
+            <a href="#faqs" class="duration-200 hover:text-indigo-400 cursor-pointer">FAQs</a>
             <button class="specialBtn">
-                <p>Satrt free today</p>
+                <p>start free today</p>
             </button>
         </nav>
     </div>
